4639f4a4cbe52d926579d08b67f147787f900fdb
aseduto <aseduto@hotmail.com>
2016-05-02 17:24:16 +0200

    add ps generation

---
+++ AdaptiveVideoTest/LoadMediaFromWs.ps1
@@ -0,0 +1,175 @@
+param(  
+		$jurl        = 'http://localhost:55625/Rai.Media.Web.Services/Json/Rest.aspx'
+      , $asset       = $null
+      , $interactive = $true
+      , $preurl      = 'http://localhost:7777/dash'
+	  , $mpd         = 'pr.mpd'
+	  , $outfile     = 'Media.xml'
+)
+
+$ErrorActionPreference = "Stop";
+
+$my_dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+if($null -eq $asset)
+{
+	Write-Error "Asset Not Provided";
+}
+
+
+function time-format()
+{
+	$fd = "yyyy-MM-ddTHHmmssfffZ";
+	[System.Globalization.DateTimeFormatInfo] $df = new-object System.Globalization.DateTimeFormatInfo
+	$df.FullDateTimePattern = $fd
+
+	return $df;
+}
+
+function parse-dt($dt)
+{
+	$df = time-format
+	return [System.DateTime]::ParseExact($dt, "F", $df, 'AssumeUniversal');
+}
+
+function to-dt([DateTime] $date)
+{
+	$df = time-format
+	return $date.ToUniversalTime().ToString("F", $df);
+}
+
+
+function curl-json($url)
+{
+ try {
+        $request = Invoke-WebRequest -Uri $url -ContentType "application/json" #-ea SilentlyContinue 
+    } 
+    catch [System.Net.WebException] {
+        $request = $_.Exception.Response
+
+    }
+
+    return $request;
+	
+}
+
+function hh($tt)
+{
+  return [TimeSpan]::FromTicks($tt).ToString();
+}
+
+
+
+
+function do-asset-leg($al, $file)
+{
+	$al | oh
+
+	
+	"<!--=============$($al.ContentType)===============-->" | out-file $file -append
+
+
+	$start = parse-dt $al.StartTime;
+	$end   = parse-dt $al.EndTime;
+
+	$rnd   = Get-Random -minimum 1 -maximum 241
+	$rl    = Get-Random -minimum 60 -maximum 7201
+
+	$step  = [TimeSpan]::FromSeconds($rnd);
+	$play_len = [TimeSpan]::FromSeconds($rl);
+
+	$len   = $end.Subtract($start);
+
+	
+
+	while($start -lt $end)
+	{
+		$play_end = $start.Add($play_len);
+
+		if($end -lt $play_end)
+		{  $play_end = $end;}
+
+		$time_range = "$(to-dt $start)/$(to-dt $play_end)";
+
+		$url = "/$($al.Name)/$($time_range)/$($al.ContentType.Replace(' ', '%20'))";
+
+		'<Url>'  | out-file $file -append
+		"	<full>$($preurl)$($url)/$mpd</full>"  | out-file $file -append
+		"   <prefix>$($preurl)$($url)</prefix>"  | out-file $file -append
+		'</Url>'  | out-file $file -append
+		
+		$url | oh
+
+		$start = $start.Add($step);
+
+		$rnd   = Get-Random -minimum 1 -maximum 241
+		$rl    = Get-Random -minimum 60 -maximum 7201
+
+		$step	  = [TimeSpan]::FromSeconds($rnd);
+		$play_len = [TimeSpan]::FromMinutes($rl);
+	}
+
+
+	
+}
+
+function do-asset($asset)
+{
+	"===============$asset===================" | oh
+	
+	$r = curl-json "$jurl/info/$asset"
+	
+	if(200 -ne $r.StatusCode)
+	{
+		"MISSING ASSET $($r.StatusCode)" | oh
+		return;
+
+		$r.Content | oh
+		Write-Error "Request Failed $($r.StatusCode)";
+		return;
+	}
+
+	$json = ConvertFrom-Json $r.Content;
+	
+	$file = "$my_dir\$outfile";
+
+	if(test-path $file)
+	{rm $file}
+
+	"<Urls>" | out-file $file -append
+
+	$json.AssetInfo | foreach {do-asset-leg $_ $file};
+
+	"</Urls>" | out-file $file -append
+
+
+}
+
+function main()
+{
+	$target = '';
+	if($null -ne $asset)
+	{
+		$target = "/$asset";
+	}
+	$r = curl "$jurl/asset$($target)" -ContentType "application/json"
+
+	if(200 -ne $r.StatusCode)
+	{
+		$r.StatusCode | oh
+		$r.Content | oh
+		Write-Error "Request Failed " + $r.StatusCode;
+		return;
+	}
+
+	$json = ConvertFrom-Json $r.Content;
+
+	$json | oh
+
+	$json.AssetList | foreach{do-asset $_} 
+}
+
+
+main
+
+
